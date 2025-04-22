f17e56b8b412d6e4ff0604291e4d1d2d0f4a2bf9
Tooj Rtn <tojorandriaII474@gmail.com>
2025-04-22 12:51:21 +0200

    CHORE: Support compound emojis in emoji checker

--- utils/emoji-checker.ts
+++ utils/emoji-checker.ts
@@ -1,5 +1,4 @@
 export function isSingleEmoji(text: string): boolean {
-  const singleEmojiRegex =
-    /^(?:\p{Emoji_Presentation}|\p{Emoji}\uFE0F|\p{Extended_Pictographic})$/u;
-  return singleEmojiRegex.test(text);
+  const emojiSequenceRegex = /^(\p{Extended_Pictographic}(?:\u200D\p{Extended_Pictographic})*)$/u;
+  return emojiSequenceRegex.test(text);
 }
